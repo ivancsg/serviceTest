5db08b9a89247931a4a2163fb8f2005fc89cf82c
Ivan Gonçalves <ivan.goncalves@integratebrasil.com>
2023-07-18 17:30:28 -0400

    carga nicial

---
+++ serviceTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace Cron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($@"Cron - Registro de execução em {DateTime.Now.ToString()}");
+
+            Thread.Sleep(2000);
+        }
+    }
+}
